11934e4f3b7b8d0f92fac4ad2461340e883f5081
CyPHer0P <prajjwal199275@gmail.com>
2020-10-28 20:42:45 +0530

    Tortoise race has also completed!

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -7,6 +7,8 @@
 
 package thread.tortoise;
 
+import javax.swing.*;
+
 public class Tortoise implements Runnable {
 
     private static final int MILESTONE = 5;
@@ -25,13 +27,39 @@ public class Tortoise implements Runnable {
     public void run() {
         for (int index2 = 0; index2 < MILESTONE; index2++) {
             switch (index2) {
+                case 0:
+                    System.out.println("The Tortoise starts from the Start Line!");
+                    break;
                 case 1:
-                    System.out.println("The Tortoise ");
+                    System.out.println("The Tortoise has reached the Cool River!");
+                    break;
                 case 2:
+                    System.out.println("The Tortoise has reached the Mountain Hill!");
+                    break;
                 case 3:
+                    System.out.println("The Tortoise has reached the Oak Tree!");
+                    System.err.println("The Tortoise is left behind from Rabbit hence Tortoise runs Continue!");
+                    try {
+                        Thread.sleep(9000L);
+                    } catch (InterruptedException e) {
+                        System.err.println("The Tortoise left behind so far!");
+                    }
+                    break;
                 case 4:
+                    System.out.println("The Tortoise has reached the Finish Line!");
+                    break;
+            }
+            try {
+                Thread.sleep(3000L);
+            } catch (InterruptedException e) {
+                System.err.println("The Tortoise has lost the path!");
             }
         }
+        JOptionPane.showConfirmDialog(
+                null,
+                "The Tortoise has Completed the race!",
+                "Tortoise",
+                JOptionPane.OK_CANCEL_OPTION
+        );
     }
 }
-
